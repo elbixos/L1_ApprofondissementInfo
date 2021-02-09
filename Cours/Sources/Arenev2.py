9865caf9ffdff512c2d5f2dbafff4b879a301737
elbixos <vpage.vincent@gmail.com>
2021-02-09 16:02:25 -0400

    avec Mago et critiques...

--- Cours/Sources/Arenev2.py
+++ Cours/Sources/Arenev2.py
@@ -1,5 +1,16 @@
 from Perso import *
 
+def virer_morts(arene):
+    new_arene = []
+    for p in arene :
+        if not p.is_alive():
+            print("..........")
+            print("Arghhhhh, honorez la memoire de ",p.name)
+        else :
+            new_arene.append(p)
+
+    return new_arene
+
 arene = []
 
 p = Barbare("Ra's al Ghul")
@@ -11,11 +22,23 @@ arene.append(p)
 p = Chevalier("Link")
 arene.append(p)
 
+p = Magicien("Merlin")
+arene.append(p)
+
+p = Magicien("Dumbledore")
+arene.append(p)
+
+p = Barbare("Dar Umbra")
+arene.append(p)
+
 # Affichage initial
 for unGars in arene:
     unGars.afficher()
 
+print("\n===========")
+print("===========")
 print ("Bienvenue dans le Thunder Dome")
+
 while len(arene) > 1 :
     print("===========")
     attaquant = random.choice(arene)
@@ -26,10 +49,7 @@ while len(arene) > 1 :
 
     attaquant.attaquer(victime)
 
-    if not victime.is_alive():
-        print("..........")
-        print("Arghhhhh, honorez la memoire de ",victime.name)
-        arene.remove(victime)
+    arene = virer_morts(arene)
 
 print("======================")
 print ("Le vainqueur du trophée des champions est :")
--- Cours/Sources/Perso.py
+++ Cours/Sources/Perso.py
@@ -16,6 +16,7 @@ class Perso:
         attaque = Attaque("lowkick", 3+ random.randint(0,1))
         self.liste_attaques.append(attaque)
 
+
     def afficher(self):
         print("==========")
         print("voici :", self.name )
@@ -30,7 +31,17 @@ class Perso:
 
 
     def get_attack_score(self):
-        return self.attack + random.randint(1,6)
+        tirage = random.randint(1,6)
+        score  = self.attack + tirage
+        statut = "standard"
+
+        if tirage == 1:
+            statut = "fail"
+
+        if tirage == 6:
+            statut = "success"
+
+        return score, statut
 
     def get_defense_score(self):
         return self.defense + random.randint(1,6)
@@ -43,23 +54,52 @@ class Perso:
 
 
     def attaquer(self,victime):
+
+        # Choix d'une attaqie
         attaque = random.choice(self.liste_attaques)
+        while not self.is_attack_possible(attaque):
+            #print("ATTAQUE IMPOSSIBLE", attaque.nom)
+            attaque = random.choice(self.liste_attaques)
+
         print("Tentative de",attaque.nom, "de" ,self.name, "sur", victime.name)
 
-        score_attaque = self.get_attack_score()
-        score_defense = victime.get_defense_score()
+        score_attaque,statut = self.get_attack_score()
 
-        if score_attaque > score_defense :
-            print("l'attaque de", self.name,"passe")
-            victime.receive_damages(attaque.degats)
+        if statut == "fail":
+            print("ECHEC CRITIQUE")
+            self.receive_damages(attaque.degats)
 
-        else:
-            print("belle esquive de ", victime.name)
+        elif statut == "success":
+            print("SUCCES CRITIQUE")
+            victime.receive_damages(2*attaque.degats)
+
+
+        else :
+            score_defense = victime.get_defense_score()
+
+            if score_attaque > score_defense :
+                print("l'attaque de", self.name,"passe")
+                victime.receive_damages(attaque.degats)
+
+            else:
+                print("belle esquive de ", victime.name)
+
+        # On applique les effets de l'attaque a l'agresseur
+        self.apply_effects_on_attacker(attaque)
 
     def is_alive(self):
         if self.hp <= 0:
             return False
         return True
+
+    def is_attack_possible(self, attaque):
+        if attaque.type == "standard":
+            return True
+        return False
+
+    def apply_effects_on_attacker(self,attaque):
+        if attaque.type == "standard":
+            pass
 
 class Barbare(Perso):
 
@@ -107,13 +147,61 @@ class Chevalier(Perso):
         super().afficher()
         print("armure :",self.armure)
 
+class Magicien(Perso):
+    def __init__(self,name):
+        super().__init__(name)
+        self.adjectives =  ["le Grand", "le Nécromancien"]
+        self.name = name + " " + random.choice(self.adjectives)
+        self.classe = "Mago"
+        self.hp-=2
+        self.attack-=1
+        self.defense-=1
+        self.mana = 8
 
+        attaque = AttaqueMagique("Boule de Feu",7, 6)
+        self.liste_attaques.append(attaque)
+
+        attaque = AttaqueMagique("Eclairs de Zardoz",4, 4)
+        self.liste_attaques.append(attaque)
+
+    def afficher(self):
+        super().afficher()
+        print("mana :",self.mana)
+
+    def is_attack_possible(self, attaque):
+        #attaque.afficher()
+
+        if attaque.type == "standard":
+            return True
+
+        if attaque.type == "magique":
+            if self.mana >= attaque.mana :
+                return True
+
+        #print("Autres cas")
+        return False
+
+    def apply_effects_on_attacker(self,attaque):
+        if attaque.type == "magique":
+            self.mana -= attaque.mana
+            print("Soustraction des points de mana")
+            print("reste",self.mana, "pour ",self.name)
 
 
 class Attaque:
     def __init__(self,nom, degats):
         self.nom = nom
         self.degats = degats
+        self.type = "standard"
 
     def afficher(self):
-        print(self.nom, " degats :",self.degats)
+        print(self.nom, "type :",self.type, "degats :",self.degats)
+
+class AttaqueMagique(Attaque):
+    def __init__(self,nom, degats,mana):
+        super().__init__(nom,degats)
+        self.type = "magique"
+        self.mana = mana
+
+    def afficher(self):
+        print(self.nom, "type :",self.type, "degats :",self.degats, "cout en mana", self.mana)
